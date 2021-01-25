cc1a396cd0cbbfdc60c7b1e92549bde05575db7c
Andre <andre.fabian.melo@gmail.com>
2021-01-24 23:23:43 -0300

    Add pwa configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
-import type { AppProps /*, AppContext */ } from 'next/app'
+import type { AppProps /*, AppContext */, NextWebVitalsMetric } from 'next/app'
 import Head from 'next/head'
 
 import GlobalStyles from 'styles/global'
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  console.log(metric)
+}
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +14,7 @@ function App({ Component, pageProps }: AppProps) {
         <title>Won</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project starter to work with Typescript, React, NextJS ans Styled Components"
